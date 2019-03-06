eb16d1586df4a1a335e99c4686c5e6f925d20223
Pauline Rudge <paulinerudge@me.com>
2019-03-06 11:16:34 +0000

    app.js set up

---
+++ client/src/app.js
@@ -0,0 +1,18 @@
+const FormView = require('./views/form_view.js')
+const ResultView = require('./views/result_view.js');
+const BucketList = require('./models/bucket_list.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const formElement = document.querySelector("form#item-input");
+  const formView = new FormView(formElement);
+  formView.bindEvents();
+
+  const  resultElement = document.querySelector("bucketlist-results");
+  const resultView = new ResultView(resultElement);
+  resultView.bindEvents();
+
+  const url = "http://localhost:3000/api/bucketlist";
+  const bucketList = new BucketList(url);
+  bucketList.bindEvents();
+  bucketList.getData();
+});
