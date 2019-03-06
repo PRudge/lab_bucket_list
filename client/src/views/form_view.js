8db6cb7c32dd9d115134c6bc20acc9d5ce75dcea
Pauline Rudge <paulinerudge@me.com>
2019-03-06 11:43:11 +0000

    form view completed

---
+++ client/src/views/form_view.js
@@ -0,0 +1,18 @@
+PubSub = require("../helpers/pub_sub.js")
+
+const FormView = function(form){
+  this.form = form;
+}
+
+FormView.prototype.bindEvents = function () {
+  this.form.addEventListener('submit',(evt) => {
+    evt.preventDefault();
+    PubSub.publish("FormView: submit", {
+      "bucket_item":evt.target.item_bucketlist.value,
+      "location":evt.target.item_location.value,
+      "image_url":evt.target.item_image.value
+    });
+  });
+};
+
+module.exports = FormView;
